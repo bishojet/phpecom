11d5e9ab64bfbce4dc1279c994ef228b0d037a6a
Bishojet Chandra Nath <bishojetchandranath@gmail.com>
2022-03-18 23:20:58 +0600

    Customize Frontend Page

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+<!-- jquery  -->
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,24 +1,11 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php include_once 'inc/header.php'; ?>
+<?php include_once 'inc/navbar.php'; ?>
 
-    <!-- Bootstrap CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
+
     <h1>Hello, world!</h1>
     <button class="btn btn-success">Bootstrap</button>
 
-    <!-- jquery  -->
-    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-
-  </body>
-</html>
+<?php include_once 'inc/footer.php'; ?>
